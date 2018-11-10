e5834c95acc390ddb87da8a5023d98174b13ecc6
Cliff Gor <clierozigy@gmail.com>
2018-11-10 13:17:57 +0300

    Add Crud Added queries which will perform crud operations

--- dbConnection.js
+++ dbConnection.js
@@ -25,6 +25,11 @@ module.exports = async () => {
     }
 }
 
+// queries to perform crud
+
 module.exports = {
-    
+    insert_todo: `INSERT INTO tbl_todo(title,completed ) VALUES(?, ?)`,
+    read_todo: `SELECT * FROM tbl_todo`,
+    update_todo: `UPDATE tbl_todo SET tbl_todo.title = ?. tb;_todo.completed =? WHERE tbl_todo.id = ?`,
+    delete_todo: `DELETE FROM tbl_todo WHERE tbl.id = ?`
 }
